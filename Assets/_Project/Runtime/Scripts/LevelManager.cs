03896dde7fd11d059ffd7decbbdc0c02019c95c4
Antoine Hanna <hannaantoinebusiness@gmail.com>
2024-09-11 11:05:22 +0200

    [ADD] Sequence to next Level

--- Assets/_Project/Runtime/Scripts/LevelManager.cs
+++ Assets/_Project/Runtime/Scripts/LevelManager.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    
+    public void ChangeLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
 }
--- Assets/_Project/Runtime/Scripts/Tiles/StairTile.cs
+++ Assets/_Project/Runtime/Scripts/Tiles/StairTile.cs
@@ -5,29 +5,10 @@ using UnityEngine.SceneManagement;
 public class StairTile : Tile
 {
     [SerializeField] private string _nextLevelName;
-    
-    //Action
-    public event UnityAction OnNextLevel;    
-    
-    //Unity Event
-    [SerializeField] private UnityEvent OnNextLevelEvent;
 
-    private void Awake()
-    {
-        OnNextLevelEvent.AddListener(() => OnNextLevel?.Invoke());
-    }
-
-    void ToNextLevel(string levelName)
-    {
-        OnNextLevelEvent?.Invoke();
-        SceneManager.LoadScene(levelName);
-    }
-
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         // Maybe add here a condition to check that its the player that is coming in the collider and not something else
-        
         OnTriggerEnteredEvent?.Invoke();
-        ToNextLevel(_nextLevelName);
     }
 }
