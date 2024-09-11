1eb1d19a0fcf94cf180795ffe27eaf2459c25c5b
Antoine Hanna <hannaantoinebusiness@gmail.com>
2024-09-11 14:06:18 +0200

    [ADD] LevelTimer

--- Assets/_Project/Runtime/Scripts/LevelManager.cs
+++ Assets/_Project/Runtime/Scripts/LevelManager.cs
@@ -1,12 +1,54 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class LevelManager : MonoBehaviour
 {
+    //Levels
     [SerializeField] private string _previousLevelName;
     [SerializeField] private string _nextLevelName;
 
+    //Menu
     [SerializeField] private EndMenu _endMenu;
+
+    //LevelTimer
+    [SerializeField] private float _levelTime;
+    private float _timer;
+
+    //Actions
+    public event UnityAction OnLevelEnded;
+    
+    //UnityEvents 
+    [SerializeField] private UnityEvent OnLevelEndedEvent;
+
+    private void Awake()
+    {
+        OnLevelEndedEvent.AddListener(()=>OnLevelEnded?.Invoke());
+    }
+
+    private void Start()
+    {
+        _timer = 0;
+    }
+
+    private void Update()
+    {
+        if (_timer < _levelTime)
+        {
+            _timer += Time.deltaTime;
+        }
+        else
+        {
+            EndLevel();
+        }
+    }
+
+    private void EndLevel()
+    {
+        OnLevelEndedEvent?.Invoke();
+    }
     
     public void FallToPreviousLevel()
     {
