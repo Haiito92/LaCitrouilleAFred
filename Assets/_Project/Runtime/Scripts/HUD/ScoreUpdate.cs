d76f6010e30da0e141901dea0eee851460983d14
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-09-12 23:12:27 +0200

    [MOD] Added score to HUD and final sequence

---
+++ Assets/_Project/Runtime/Scripts/HUD/ScoreUpdate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class ScoreUpdate : MonoBehaviour
+{
+    [SerializeField]TMP_Text _scoreDisplay;
+    private UnityEvent OnChagedScore;
+
+    private void Start()
+    {
+        UpdateScore();
+    }
+    // Update is called once per frame
+    void UpdateScore()
+    {
+        _scoreDisplay.text = $"Score : {GameManager.Instance.Score}";
+    }
+}
