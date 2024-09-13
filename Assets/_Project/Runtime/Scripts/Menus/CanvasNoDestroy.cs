e46bda43d5489fc44404ad4a38bec6b8756ce91a
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2024-09-13 02:06:54 +0200

    [ADD] ButtonEndScreen

--- Assets/_Project/Runtime/Scripts/Menus/CanvasNoDestroy.cs
+++ Assets/_Project/Runtime/Scripts/Menus/CanvasNoDestroy.cs
@@ -1,9 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasNoDestroy : MonoBehaviour
 {
+    private CanvasNoDestroy _instance;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
